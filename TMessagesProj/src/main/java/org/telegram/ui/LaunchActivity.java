0e80199c494e4f69f4586136f383f8a96c95b1d5
eval Nya <60149113+nexplorer-3e@users.noreply.github.com>
2022-03-19 09:58:57 +0800

    try to reimplement tg:nya

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -2203,7 +2203,8 @@ public class LaunchActivity extends BasePermissionsActivity implements ActionBar
                                         newContactPhone = data.getQueryParameter("phone");
                                         newContact = true;
                                     } else if (url.startsWith("tg:meow") || url.startsWith("tg://meow") || url.startsWith("tg:nya") || url.startsWith("tg://nya") || url.startsWith("tg:miao") || url.startsWith("tg://miao")) {
-                                        showBulletin(factory -> factory.createErrorBulletin(LocaleController.getString("Nya", R.string.Nya)));
+                                        // showBulletin(factory -> factory.createErrorBulletin(LocaleController.getString("Nya", R.string.Nya)));
+                                        NotificationCenter.getGlobalInstance().postNotificationName(NotificationCenter.showBulletin, Bulletin.TYPE_ERROR, LocaleController.getString("Nya", R.string.Nya));
                                     } else {
                                         unsupportedUrl = url.replace("tg://", "").replace("tg:", "");
                                         int index;
