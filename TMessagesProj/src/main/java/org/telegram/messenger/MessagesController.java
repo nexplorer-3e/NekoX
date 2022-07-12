b24229544651ef99f3eff66be7845f19226534cf
luvletter2333 <luvletter2333@gmail.com>
2022-07-13 01:10:47 +0800

    [fork] fix local premium

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -523,7 +523,7 @@ public class MessagesController extends BaseController implements NotificationCe
     }
 
     public boolean isPremiumUser(TLRPC.User currentUser) {
-        return NekoConfig.localPremium.Bool() || (!premiumLocked && currentUser.premium);
+        return (!premiumLocked && currentUser.premium);
     }
 
     public boolean didPressTranscribeButtonEnough() {
